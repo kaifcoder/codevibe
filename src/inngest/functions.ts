daae944a495727842d8ffba64deba904c7de0ddc
kaifcoder <kaifmohd2014@gmail.com>
2025-08-03 22:10:05 +0530

    feat: add E2B sandbox integration and enhance coding agent functionality

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,8 @@
 import { inngest } from "./client";
 import { chatWithAI } from "../lib/ai-core-chat";
 import { invokeNextJsAgent } from "@/lib/nextjs-coding-agent";
+import { Sandbox } from '@e2b/code-interpreter'
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -21,18 +23,37 @@ export const helloWorld = inngest.createFunction(
 export const codingAgent = inngest.createFunction(
   {id: "coding-agent" },
   {event: "test/coding.agent"},
-  async ({ event }) => {
-    const userPrompt = event.data?.message || "How do I create a Next.js page?";
-    try {
-      const agentResult = await invokeNextJsAgent(userPrompt);
-      return { 
-        aiResponse: agentResult.response
-      };
-    } catch (err) {
-      return { 
-        aiResponse: "An error occurred while processing your request.",
-        error: err instanceof Error ? err.message : String(err)
-      };
-    }
+  async ({ event, step }) => {
+
+    const res = await step.run("Invoke-agent", async () => {
+      const userPrompt = event.data?.message || "How do I create a Next.js page?";
+      try {
+        const agentResult = await invokeNextJsAgent(userPrompt);
+        return { 
+          aiResponse: agentResult.response
+        };
+      } catch (err) {
+        return { 
+          aiResponse: "An error occurred while processing your request.",
+          error: err instanceof Error ? err.message : String(err)
+        };
+      }
+    })
+   const sbxId =  await step.run("get-sandbox-id", async () => {
+      const sbx = await Sandbox.create('codevibe-test');
+      return sbx.sandboxId;
+    })
+
+    const sbxUrl = await step.run("get-sandbox-url", async () => {
+      const sbx =  await getSandbox(sbxId);
+      const host = sbx.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { 
+      message: res.aiResponse,
+      sandboxUrl: sbxUrl,
+    };
   }
 )
+
